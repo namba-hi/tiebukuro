77ba2780a028e73a0b8842798453e14a7bf5a4c0
namba-hi <howdoyoulikewednesday1021hdon@ezweb.ne.jp>
2020-02-04 00:31:19 +0900

    gem-install

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,16 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    Post.create
+  end
+
+  def edit
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200203071720) do
+ActiveRecord::Schema.define(version: 20200203095055) do
+
+  create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "title",      limit: 65535, null: false
+    t.text     "content",    limit: 65535, null: false
+    t.integer  "user_id",                  null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 20200203071720) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "posts", "users"
 end
