4432b089486fe1c51c8a01f7a31cbfa62f594c2e
namba-hi <howdoyoulikewednesday1021hdon@ezweb.ne.jp>
2020-02-04 19:34:09 +0900

    user-name 表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200204014748) do
+ActiveRecord::Schema.define(version: 20200204093739) do
 
   create_table "posts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "title",      limit: 65535, null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20200204014748) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
