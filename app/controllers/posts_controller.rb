6a226d3fbfb520aa51c61fb10247d34da3b8c304
namba-hi <howdoyoulikewednesday1021hdon@ezweb.ne.jp>
2020-02-04 15:52:32 +0900

    new-view create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,25 @@ class PostsController < ApplicationController
   end
 
   def new
-    @posts = Post.new
+    @post = Post.new
   end
 
   def create
-    Post.create
+    @post = Post.new(post_params)
+    tag_list = params[:post][:tag_name].split(",")
+    @blog.save_blogs(tag_list)
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :tag_ids).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
