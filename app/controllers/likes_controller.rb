806e7af516699eb9562d774dd6a3f829f5b2d6a5
namba-hi <howdoyoulikewednesday1021hdon@ezweb.ne.jp>
2020-03-15 18:24:18 +0900

    likecontroller_edit

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,28 @@
 class LikesController < ApplicationController
+  before_action :set_post, only: [:create, :destroy]
+
   def create
-    @like = current_user.likes.create(post_id: params[:post_id])
-  end
+    @post = Post.find(params[:post_id])
+    @like = current_user.likes.create(post_id: params[:post_id], user_id: current_user.id)
+    @like.save!
+    @likes = Like.where(post_id: params[:post_id])
+
+  end 
 
   def destroy
+    @post = Post.find(params[:post_id])
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
+    @likes = Like.where(post_id: params[:post_id])
   end
+
+  private
+  def like_params
+    params.permit(post_id: params[:post_id], user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+    
 end
