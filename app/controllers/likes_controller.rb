322863e64af9675ffb1007cae36afe6923b7c225
namba-hi <howdoyoulikewednesday1021hdon@ezweb.ne.jp>
2020-03-15 18:53:06 +0900

    delete_code

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,15 +3,11 @@ class LikesController < ApplicationController
 
   def create
     @like = current_user.likes.create(post_id: params[:post_id], user_id: current_user.id)
-    # @like.save!
-    # @likes = Like.where(post_id: params[:post_id])
-
   end 
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    # @likes = Like.where(post_id: params[:post_id])
   end
 
   private
